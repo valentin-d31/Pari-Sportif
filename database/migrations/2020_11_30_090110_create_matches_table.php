83abd56beff4c3fb61225a93708668da1cff6747
valentin arpin <valentin.arpin@ynov.com>
2020-11-30 10:15:24 +0100

    creation bdd Match

--- database/migrations/2020_11_30_090110_create_matches_table.php
+++ database/migrations/2020_11_30_090110_create_matches_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminsTable extends Migration
+class CreateMatchesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,13 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('matches', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('pays');
+            $table->unsignedBigInteger('equipe_id');
+            $table->integer('cote');
+            $table->integer('duree');
             $table->timestamps();
         });
     }
@@ -26,6 +31,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('matches');
     }
 }
